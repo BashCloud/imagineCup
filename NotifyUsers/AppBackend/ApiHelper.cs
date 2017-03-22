f5831f8c5515d2b536e0fb9fd03e32551cdefbff
Pulkit Aggarwal <pulkit.cloud@gmail.com>
2017-03-22 14:53:36 +0530

    ApiHelper.cs added to remove compile time errors.

--- NotifyUsers/AppBackend/ApiHelper.cs
+++ NotifyUsers/AppBackend/ApiHelper.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿using AppBackend.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
-using Microsoft.AspNet.SignalR;
 
 namespace AppBackend
 {
     public class ApiHelper
     {
-        public static readonly string ApiEndpoint = "https://spatial.virtualearth.net/REST/v1/data/e571c65f66de488ea9dde02ff57585b0/geoFences/TestBoundaries?spatialFilter=intersects(%27POINT%20({0}%20{1})%27)&$format=json&key={2}";
-        public static readonly string ApiKey = "bneAWOJdktXwP5i4cgWe~cf4jAcJUeD9X3nQLXEHjXQ~As-yWK2_R7Wxaehn8I3JIaTSkORIrlIQP66prVtVodG5MzrfC9p38sUaoasx25ih";
+        public static readonly string ApiEndpoint = "{YOUR_QUERY_ENDPOINT}?spatialFilter=intersects(%27POINT%20({0}%20{1})%27)&$format=json&key={2}";
+        public static readonly string ApiKey = "{YOUR_API_KEY}";
 
         public static bool IsPointWithinBounds(string longitude, string latitude)
         {
